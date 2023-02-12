065fd0b661079889c81e7eff30880956589e8b3d
AnthonyLee <anthony@calglobal.com>
2023-02-13 06:50:56 +0800

    add 2nd test case

--- src/test/java/com/techgames/MarsRoverTest.java
+++ src/test/java/com/techgames/MarsRoverTest.java
@@ -12,4 +12,11 @@ public class MarsRoverTest {
         Plateau plateau = new Plateau (5,5);
         assertEquals ("1 3 N", marsRover.navigate("LMLMLMLMM"));
     }
+
+    @Test
+    public void chk2ndRoverNavigation() {
+        MarsRover marsRover = new MarsRover("M2", 3, 3,'E');
+        Plateau plateau = new Plateau (5,5);
+        assertEquals ("5 1 E", marsRover.navigate("MMRMMRMRRM"));
+    }
 }
