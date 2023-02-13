0396c6f8c7507b66bd42f4a243c6af39b683df66
AnthonyLee <anthony@calglobal.com>
2023-02-14 07:56:12 +0800

    add file input for test cases

--- src/test/java/com/techgames/MarsRoverTest.java
+++ src/test/java/com/techgames/MarsRoverTest.java
@@ -1,6 +1,8 @@
 package com.techgames;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -25,5 +27,13 @@ public class MarsRoverTest {
         MarsRover marsRover = new MarsRover("M1", 3, 3,'E');
         Plateau plateau = new Plateau (5,5);
         assertEquals ("5 3 E", marsRover.navigate("MMM", plateau));
+    }
+    //Unit test for one Rover
+    @ParameterizedTest
+    @CsvFileSource(resources = "/input.csv", numLinesToSkip = 1)
+    public void chkCommandForOneRover(String expected, String command) {
+        MarsRover marsRover = new MarsRover("M1", 1, 2,'N');
+        Plateau plateau = new Plateau (5,5);
+        assertEquals (expected, marsRover.navigate(command, plateau));
     }
 }
